1cd8139dcab1e5cb6a6def7bb4ccdd6eab545775
Gregor Middell <gregor@middell.net>
2017-01-26 17:42:35 +0100

    Project setup

---
+++ src/main/resources/archetype-resources/src/test/java/AppTest.java
@@ -0,0 +1,19 @@
+package ${package};
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test skeleton.
+ */
+public class AppTest {
+
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void app() {
+        assertTrue(true);
+    }
+}
